fa2db225c276ba6a128fe43a6afa6ccbc9e8892c
Amauri <hamauri@aol.com>
2024-11-18 17:06:34 -0800

    added auth action

---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        console.log('credentials', credentials);
+        return null;
+      },
+    }),
+  ],
+});

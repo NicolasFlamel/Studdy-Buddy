58aa2d6a4c77272865bbd0bfec0cad750b1e79d4
Amauri <hamauri@aol.com>
2024-11-25 16:14:57 -0800

    created updateAssessment action
    - formSchema created for submitting to update assessment

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,12 +1,84 @@
 'use server';
 
 import { signIn } from 'auth';
+import { updateUserScores } from 'drizzle';
 import { AuthError } from 'next-auth';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
-export async function authenticate(
+export type State = {
+  errors?: {
+    vanillaJs?: string[];
+    mySql?: string[];
+    nodeJs?: string[];
+    express?: string[];
+    oop?: string[];
+  };
+  message?: string | null;
+};
+
+const FormSchema = z.object({
+  vanillaJs: z.coerce
+    .number({ invalid_type_error: 'Please select a value.' })
+    .gt(0)
+    .lt(6),
+  mySql: z.coerce
+    .number({ invalid_type_error: 'Please select a value.' })
+    .gt(0)
+    .lt(6),
+  nodeJs: z.coerce
+    .number({ invalid_type_error: 'Please select a value.' })
+    .gt(0)
+    .lt(6),
+  express: z.coerce
+    .number({ invalid_type_error: 'Please select a value.' })
+    .gt(0)
+    .lt(6),
+  oop: z.coerce
+    .number({ invalid_type_error: 'Please select a value.' })
+    .gt(0)
+    .lt(6),
+});
+
+const UpdateAssessment = FormSchema.omit({});
+
+export const updateAssessment = async (
+  userId: string,
+  prevState: State,
+  formData: FormData,
+): Promise<State> => {
+  const validatedFields = UpdateAssessment.safeParse({
+    vanillaJs: formData.get('vanillaJs'),
+    mySql: formData.get('mySql'),
+    nodeJs: formData.get('nodeJs'),
+    express: formData.get('express'),
+    oop: formData.get('oop'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Assessment.',
+    };
+  }
+
+  const scores = validatedFields.data;
+
+  try {
+    updateUserScores(userId, scores);
+  } catch {
+    return { message: 'Database Error: Failed to Update Assessment.' };
+  }
+
+  revalidatePath('/assessments');
+  redirect('/');
+};
+
+export const authenticate = async (
   prevState: string | undefined,
   formData: FormData,
-) {
+) => {
   try {
     const username = formData.get('username');
     const password = formData.get('password');
@@ -24,8 +96,4 @@ export async function authenticate(
     }
     throw error;
   }
-}
-
-export const updateAssessment = async (formData: FormData) => {
-  console.log(formData);
 };

34fa5468fb104cb00041abdfa000fa5b3795f3bf
Amauri <hamauri@aol.com>
2024-11-26 14:52:42 -0800

    updated 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,23 @@
-import Link from 'next/link';
+import { Link } from '@nextui-org/link';
+import { Card, CardHeader, CardBody, CardFooter } from '@nextui-org/card';
+import { Divider } from '@nextui-org/divider';
 
 const NotFound = () => {
   return (
-    <section>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <section className="flex justify-center m-4">
+      <Card>
+        <CardHeader>
+          <h2>404 - Not Found</h2>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>Could not find requested resource</p>
+        </CardBody>
+        <Divider />
+        <CardFooter className="justify-end">
+          <Link href="/">Return Home</Link>
+        </CardFooter>
+      </Card>
     </section>
   );
 };

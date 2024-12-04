4a90c0e64405cb84a46356b7db69bf965d3b993a
Amauri <hamauri@aol.com>
2024-12-04 10:53:05 -0800

    added ChatSkeleton

--- src/components/skeletons.tsx
+++ src/components/skeletons.tsx
@@ -1,7 +1,27 @@
+import ChatBubble from './chat/chat-bubble';
+import { Skeleton } from '@nextui-org/react';
+
 // Loading animation
 const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
 
 export const AssessmentEditFormSkeleton = () => {
   return <div className={shimmer + ''}></div>;
+};
+
+export const ChatSkeleton = () => {
+  return (
+    <div>
+      <div>
+        <ChatBubble self>
+          <Skeleton className="rounded-lg">Loading...</Skeleton>
+        </ChatBubble>
+      </div>
+      <div>
+        <ChatBubble>
+          <Skeleton className="rounded-lg">Loading...</Skeleton>
+        </ChatBubble>
+      </div>
+    </div>
+  );
 };

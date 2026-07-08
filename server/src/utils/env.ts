b02e53a015d6bcfa7568acb9d8229c0753dbc6ae
Amauri <hamauri@aol.com>
2026-07-08 12:25:18 -0700

    fix(env): removed unused envs

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -3,16 +3,11 @@ import { z } from 'zod';
 const nonEmptyString = z.string().trim().min(1);
 
 const envSchema = z.object({
-  PORT: nonEmptyString.optional(),
   DATABASE_URL: nonEmptyString,
+  AUTH_SECRET: nonEmptyString,
+  PORT: nonEmptyString.optional(),
   LOG_LEVEL: nonEmptyString.optional(),
   NODE_ENV: nonEmptyString.optional(),
-  AUTH_SECRET: nonEmptyString,
-  DB_NAME_PSQL: nonEmptyString,
-  DB_USER_PSQL: nonEmptyString,
-  DB_PASSWORD_PSQL: nonEmptyString,
-  DB_HOST: nonEmptyString,
-  DB_PORT: nonEmptyString,
 });
 
 type Env = z.infer<typeof envSchema>;
